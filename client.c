c5f6e617a8af49c40415789202b6a137487f96a5
Shriya Seth <123366111+Shriyaseth25@users.noreply.github.com>
2024-09-04 12:41:07 +0530

    Update client.c

--- client.c
+++ client.c
@@ -4,6 +4,8 @@
 #include <unistd.h>
 #include <arpa/inet.h>
 #include <pthread.h>
+#include "encryption.h"
+#include "client_header.h"
 
 #define PORT 8080
 #define BUFFER_SIZE 1024
@@ -11,56 +13,6 @@
 
 const char *encryption_key = "your_key";
 
-// XOR encryption/decryption function
-void xor_encrypt_decrypt(char *data, const char *key) {
-    int data_len = strlen(data);
-    int key_len = strlen(key);
-
-    for (int i = 0; i < data_len; ++i) {
-        data[i] ^= key[i % key_len];
-    }
-}
-
-// Function to receive messages from the server
-void *receive_messages(void *sock) {
-    int sockfd = *((int *)sock);
-    char buffer[BUFFER_SIZE];
-
-    while (1) {
-        int nbytes = recv(sockfd, buffer, sizeof(buffer), 0);
-        if (nbytes > 0) {
-            buffer[nbytes] = '\0';
-            xor_encrypt_decrypt(buffer, encryption_key);  // Decrypt the received message
-            printf("Server: %s", buffer);
-        } else {
-            printf("Disconnected from server.\n");
-            close(sockfd);
-            pthread_exit(NULL);
-        }
-    }
-}
-
-// Function to handle user login
-void login(int sockfd) {
-    char username[MAX_LEN], password[MAX_LEN], buffer[MAX_LEN];
-
-    // Request login credentials
-    printf("Enter username: ");
-    fgets(username, MAX_LEN, stdin);
-    username[strcspn(username, "\n")] = 0;  // Remove newline
-
-    printf("Enter password: ");
-    fgets(password, MAX_LEN, stdin);
-    password[strcspn(password, "\n")] = 0;  // Remove newline
-
-    // Send login details to the server
-    snprintf(buffer, MAX_LEN, "LOGIN %s %s", username, password);
-    send(sockfd, buffer, strlen(buffer), 0);
-
-    // Receive server response
-    recv(sockfd, buffer, MAX_LEN, 0);
-    printf("%s\n", buffer);
-}
 
 int main() {
     int sock = 0;
