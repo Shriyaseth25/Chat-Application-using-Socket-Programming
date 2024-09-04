be796b965032fe48fc10b6d24aaa9c85d98060e1
Shriya Seth <123366111+Shriyaseth25@users.noreply.github.com>
2024-09-04 12:42:23 +0530

    Update server.c

--- server.c
+++ server.c
@@ -8,6 +8,8 @@
 #include <unistd.h>
 #include <arpa/inet.h>
 #include <pthread.h>
+#include "encryption.h"
+#include "server_header.h"
 
 
 // defines port number, memory of buffer, the max number of clients that can connect simultaneously and lengths for username and password
@@ -36,126 +38,6 @@ client_t *clients[MAX_CLIENTS];
 pthread_mutex_t clients_mutex = PTHREAD_MUTEX_INITIALIZER;
 const char *encryption_key = "encryption_key";
 
-// Function to check if the user exists
-int user_exists(const char *username, const char *password) {
-    FILE *file = fopen("users.txt", "r");
-    if (!file) {
-        return 0;
-    }
-
-    User user;
-    while (fscanf(file, "%s %s", user.username, user.password) != EOF) {
-        if (strcmp(user.username, username) == 0 && strcmp(user.password, password) == 0) {
-            fclose(file);
-            return 1;  // User exists
-        }
-    }
-
-    fclose(file);
-    return 0;  // User not found
-}
-
-// Function to register a new user
-int register_user(const char *username, const char *password) {
-    if (user_exists(username, password)) {
-        return 0;  // User already exists
-    }
-
-    FILE *file = fopen("users.txt", "a");
-    if (!file) {
-        return 0;
-    }
-
-    fprintf(file, "%s %s\n", username, password);
-    fclose(file);
-    return 1;  // Registration successful
-}
-
-// XOR encryption/decryption function (reversible)
-// it takes the msg and encrypts it using the above defined encryption message
-void xor_encrypt_decrypt(char *data, const char *key) {
-    int data_len = strlen(data);
-    int key_len = strlen(key);
-
-    for (int i = 0; i < data_len; ++i) {
-        data[i] ^= key[i % key_len];
-    }
-}
-
-// function to broadcast the msg send by any client along with encryption
-void broadcast_message(char *message, int sender_sockfd) {
-    pthread_mutex_lock(&clients_mutex);
-    for (int i = 0; i < MAX_CLIENTS; ++i) {
-        if (clients[i] && clients[i]->sockfd != sender_sockfd) {
-            char encrypted_message[BUFFER_SIZE];
-            strcpy(encrypted_message, message);
-            xor_encrypt_decrypt(encrypted_message, encryption_key);
-            send(clients[i]->sockfd, encrypted_message, strlen(encrypted_message), 0);
-        }
-    }
-    pthread_mutex_unlock(&clients_mutex);
-}
-
-// function to manage interaction between server and individual client
-// handles user authentication and client communication
-void *handle_client(void *arg) {
-    client_t *cli = (client_t *)arg;
-    char buffer[BUFFER_SIZE];
-    int nbytes;
-
-    // Authentication process
-    while (1) {
-        // Receive authentication data
-        if ((nbytes = read(cli->sockfd, buffer, sizeof(buffer))) <= 0) {
-            close(cli->sockfd);
-            free(cli);
-            pthread_exit(NULL);
-        }
-
-        buffer[nbytes] = '\0';
-        char *command = strtok(buffer, " ");
-        char *username = strtok(NULL, " ");
-        char *password = strtok(NULL, " ");
-
-        if (strcmp(command, "REGISTER") == 0) {
-            if (register_user(username, password)) {
-                send(cli->sockfd, "Registration successful\n", 24, 0);
-            } else {
-                send(cli->sockfd, "User already exists\n", 20, 0);
-            }
-        } else if (strcmp(command, "LOGIN") == 0) {
-            if (user_exists(username, password)) {
-                strcpy(cli->username, username);
-                send(cli->sockfd, "Login successful\n", 18, 0);
-                break;
-            } else {
-                send(cli->sockfd, "Invalid credentials\n", 20, 0);
-            }
-        }
-    }
-
-    // Handle client communication
-    while ((nbytes = read(cli->sockfd, buffer, sizeof(buffer))) > 0) {
-        buffer[nbytes] = '\0';
-        xor_encrypt_decrypt(buffer, encryption_key);  // Decrypt the received message
-
-        printf("%s: %s", cli->username, buffer);
-        broadcast_message(buffer, cli->sockfd);  // Broadcast to other clients
-    }
-
-    // Client disconnected
-    close(cli->sockfd);
-    pthread_mutex_lock(&clients_mutex);
-    for (int i = 0; i < MAX_CLIENTS; ++i) {
-        if (clients[i] && clients[i]->sockfd == cli->sockfd) {
-            clients[i] = NULL;
-            break;
-        }
-    }
-    pthread_mutex_unlock(&clients_mutex);
-    free(cli);
-    pthread_exit(NULL);
-}
 
 int main() {
     // variables to store file descriptor of server socket and client socket, and structure that stores the server's address info.
